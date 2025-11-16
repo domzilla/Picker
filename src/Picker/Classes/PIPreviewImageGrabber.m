122fe5e4e7a562a416e6f6db18b09262b271001e
Dominic Rodemer <dominic.rodemer@me.com>
2025-11-16 09:32:26 +0100

    Code formatting

--- src/Picker/Classes/PIPreviewImageGrabber.m
+++ src/Picker/Classes/PIPreviewImageGrabber.m
@@ -13,16 +13,20 @@
 @implementation PIPreviewImageGrabber
 
 + (NSImage *)imageForLocation:(NSPoint)mouseLocation;
-{    
+{
+    NSLog(@"%@", NSStringFromPoint(mouseLocation));
+    
     CGRect imageRect = CGRectMake(mouseLocation.x - kPIScreenGrabberImageWidth / 2,
                                   mouseLocation.y - kPIScreenGrabberImageHeight / 2,
                                   kPIScreenGrabberImageWidth,
                                   kPIScreenGrabberImageHeight);
     
-    CGImageRef imageRef = CGWindowListCreateImage(imageRect, kCGWindowListOptionOnScreenOnly, kCGNullWindowID, kCGWindowImageShouldBeOpaque);
-    
-    NSImage *image = [[NSImage alloc] initWithCGImage:imageRef size:NSMakeSize(kPIScreenGrabberImageWidth, kPIScreenGrabberImageHeight)];
-    
+    CGImageRef imageRef = CGWindowListCreateImage(imageRect,
+                                                  kCGWindowListOptionOnScreenOnly,
+                                                  kCGNullWindowID,
+                                                  kCGWindowImageShouldBeOpaque);
+    NSImage *image = [[NSImage alloc] initWithCGImage:imageRef
+                                                 size:NSMakeSize(kPIScreenGrabberImageWidth, kPIScreenGrabberImageHeight)];
     CGImageRelease(imageRef);
     
     return image;
